1eb07b3f03779d7aaa1627630f50c9ab3b494c74
Alex O'Konski <alexanderokonski@gmail.com>
2011-04-30 10:01:13 +0000

    Fix purge_gameservers.py

--- purge_gameservers.py
+++ purge_gameservers.py
@@ -8,7 +8,7 @@ import logging
 
 # set up the pylons environment
 
-conf = appconfig('config:amazon.ini', relative_to='.')
+conf = appconfig('config:amazon.ini', relative_to='/home/alex/gamelion')
 load_environment(conf.global_conf, conf.local_conf)
 
 # delete a server if it hasn't been successfully
